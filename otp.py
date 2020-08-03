3de69540d1913de25eae8e45bce127bdbc20b208
krishnadinesh <69156413+krishnadinesh@users.noreply.github.com>
2020-08-03 22:16:00 +0530

    Delete otp.py

--- otp.py
+++
@@ -1,9 +0,0 @@
-import math,random
-def otp():
-    dig="1234567890"
-    o=" "
-    for i in range(4):
-        o+=dig[math.floor(random.random()*10)]
-    return o
-if __name__ == "__main__":
-    print(otp())
